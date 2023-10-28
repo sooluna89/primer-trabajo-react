65016bd7d9832cf209ce98aff0ac7e7e93386a81
Sabrina Delgado <sabrina3110@gmail.com>
2023-10-28 17:07:29 -0300

    first commit

---
+++ src/componentes/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ItemListContainer = ({ greeting }) => {
+    return (
+        <main>
+            <h2 className='text-center m-5'> {greeting} </h2>
+
+        </main>
+    )
+}
+
+export default ItemListContainer
